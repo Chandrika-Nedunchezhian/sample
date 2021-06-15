4fa382c586d341b9e36ab3334cbc187143e8d7c4
Chandrika-Nedunchezhian <chandrikanedunchezhian@gmail.com>
2021-06-15 16:25:20 +0530

    fbcommit

---
+++ src/org/Fb/FaceBook.java
@@ -0,0 +1,10 @@
+package org.Fb;
+
+public class FaceBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("Welcome to FACEBOOK");
+	}
+
+}
