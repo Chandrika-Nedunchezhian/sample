9909556a02a18fe035d5b75bc34b403f7c7f7043
Chandrika-Nedunchezhian <chandrikanedunchezhian@gmail.com>
2021-06-15 20:21:19 +0530

    faceupdate

--- src/org/Fb/FaceBook.java
+++ src/org/Fb/FaceBook.java
@@ -5,6 +5,7 @@ public class FaceBook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            System.out.println("Welcome to FACEBOOK");
+           System.out.println("News feed");
 	}
 
 }
